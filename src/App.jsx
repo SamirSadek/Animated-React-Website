946ef56292bd80e28e9574e9b4e15c2000d65f76
Samir Sadek <samirsadek2009@gmail.com>
2025-03-07 01:38:22 +0600

    Navbar and About Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Hero from "./components/Hero";
+import NavBar from "./components/Navbar";
+import About from "./components/About";
 
 function App() {
   return <main className="relative min-h-screen w-screen overflow-x-hidden">
+    <NavBar/>
     <Hero></Hero>
+    <About/>
   </main>;
 }
 
