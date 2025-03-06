4060f7e52760c093bcdf0396d18d01be4feedbea
Samir Sadek <samirsadek2009@gmail.com>
2025-03-07 01:41:35 +0600

    Feature Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Hero from "./components/Hero";
 import NavBar from "./components/Navbar";
 import About from "./components/About";
+import Features from "./components/Features";
 
 function App() {
   return <main className="relative min-h-screen w-screen overflow-x-hidden">
     <NavBar/>
     <Hero></Hero>
     <About/>
+    <Features/>
   </main>;
 }
 
